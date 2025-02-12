f5de86e4514ccd963301374edf3cdb4a58fe6d30
bmjones92 <brendanjonescs@gmail.com>
2025-02-11 22:24:42 -0500

    Added support for sidebar subsection labels.

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -8,6 +8,7 @@ use Skin;
 class Hooks {
 
 	/**
+     * Hook which is called before the page is displayed.
 	 * @param $out OutputPage
 	 * @param $skin Skin
 	 * @return void
@@ -18,6 +19,24 @@ class Hooks {
 		if ($theme !== null) {
 			$out->addBodyClasses('theme-' . $theme);
 		}
+    }
+
+    /**
+     * Hook which is called when the skin is building the sidebar.
+     * @param Skin $skin
+     * @param $bar
+     * @return void
+     */
+    public static function onSkinBuildSidebar(Skin $skin, &$bar): void {
+        foreach ($bar as &$section) {
+            foreach ($section as &$item) {
+                # Items with empty page targets should be rendered as labels instead of links.
+                if ($item['href'] === '') {
+                    $item['class'] = 'sidebar-sub-section-label';
+                    unset($item['href']);
+                }
+            }
+        }
     }
 
 }
